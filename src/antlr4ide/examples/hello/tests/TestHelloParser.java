6842c567b2da7ccfa8fb2b59dc1a85ae87435384
Scott Finnie <scott.finnie@gmail.com>
2015-03-12 23:02:18 +0000

    remove unnecessary import

--- src/antlr4ide/examples/hello/tests/TestHelloParser.java
+++ src/antlr4ide/examples/hello/tests/TestHelloParser.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
